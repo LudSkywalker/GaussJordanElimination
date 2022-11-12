9116a4800989bc45785fe1939a453793a809cda1
Mateoo0003 <mateoosp.003@gmail.com>
2022-11-12 11:03:49 -0500

    for en función multiplicar

--- GaussJordan.py
+++ GaussJordan.py
@@ -1,4 +1,5 @@
 class GaussJordanRow():
+    
     def write_row(self,row,new_row):
         pass
 
@@ -7,18 +8,16 @@ class GaussJordanRow():
         
     def __rshift__(self,other):
         self.write_row(self,other)
-
+        
+    def __mul__(self,other):
+        product=[]
+        for i in self:
+            product.append(self.data[i]*other)
 
 class GaussJordanMatrix():
 
     rows=[]
-
     def __init__(self, arr):
 
         for i in range(len(arr)):
             self.rows.append(GaussJordanRow)
-
-        
-
-
-
