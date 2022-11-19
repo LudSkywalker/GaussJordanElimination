7404b2ecf6c6d4873fdf7e90462594079d17046d
LudSkywalker <ludingnumpaque@gmail.com>
2022-11-19 10:19:45 -0500

    Added test for elimination

--- GaussJordan.py
+++ GaussJordan.py
@@ -48,14 +48,30 @@ class GaussJordanMatrix():
             for col in row.data:
                 print(str(col)+",",end="")
             print()
+
+    def elimination(self,arr):
+        '''
+        Debe tomar la matrix con una lista de resultados y halla el valor de cada variable
+        >>> GaussJordanMatrix([[1,0],[1,1]]).elimination([1,0])
+        [{'name':'entrada', 'data':[[1,0,1],[1,1,0]]},{'name':'paso 1', 'data':[[1,0,1],[0,1,-1]]}]
+
+        >>> GaussJordanMatrix([[2,0],[1,1]],[1,0])
+        [{'name':'entrada', 'data':[[2,0,1],[1,1,0]]},{'name':'paso 1', 'data':[[1,0,0.5],[1,1,0]]},{'name':'paso 2', 'data':[[1,0,0.5],[0,1,-0.5]]}]
+
+        >>> GaussJordanMatrix([[0,2],[1,1]],[1,0])
+        [{'name':'entrada', 'data':[[0,2,1],[1,1,0]]},{'name':'paso 1', 'data':[[1,1,0],[0,2,1]]},{'name':'paso 2', 'data':[[1,1,0],[0,1,0.5]]},{'name':'paso 3', 'data':[[1,0,-0.5],[0,1,0.5]]}]
+
+        '''
+        pass
+        
         
 
 mat=GaussJordanMatrix([[1,2,3],[4,5,6],[7,8,9]])
-newrow=mat.rows[0]*5
-newrow.show_row()
+# newrow=mat.rows[0]*5
+# newrow.show_row()
+
 # newrow=mat.rows[0]+mat.rows[1]
 # newrow.show_row()
+# mat.rows[0] << mat.rows[0]*5
+# mat.rows[0].show_row()
 # mat.show_matrix()
-
-
-
